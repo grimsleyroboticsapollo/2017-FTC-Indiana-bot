0ca297c255d5e829207f2fb65e3646f053a80ddb
Jens Koeplinger <jens@prisage.com>
2018-01-26 08:01:05 -0500

    remmoved an older TODO that has since been implemented

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DebugCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DebugCode.java
@@ -2,10 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.Gamepad;
 
-/**
- * TODO #JK this calculation works, make it part of the main program and implement a speedMult variable there.
- */
-
 public class DebugCode {
     public static double speedMult (Gamepad gamepad) {
         double leftTrigger = gamepad.left_trigger;
